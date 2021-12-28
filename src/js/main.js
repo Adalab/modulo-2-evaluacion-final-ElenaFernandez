df1606ba985eebdd78f8f646dd6c154f13aef1cb
ElenaFernandez <elena.fernandez92@gmail.com>
2021-12-28 18:39:23 +0100

    add JS structure

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,12 @@
 'use strict';
 
+//VARIABLES GLOBALES
 let inputText = document.querySelector('.js-input-text');
 const btnSearch = document.querySelector('.js-btn-search');
 const btnReset = document.querySelector('.js-btn-reset');
+const animeList = document.querySelector('.js-list');
+const listFavourites = document.querySelector('.js-favourites');
+let serieList = [];
+
 
 console.log('>> Ready :)');
