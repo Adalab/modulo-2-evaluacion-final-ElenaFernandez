229de0961632df45277624cd4d99483601fcd009
ElenaFernandez <elena.fernandez92@gmail.com>
2021-12-28 12:29:42 +0100

    add adalaber web kit starter

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let inputText = document.querySelector('.js-input-text');
+const btnSearch = document.querySelector('.js-btn-search');
+const btnReset = document.querySelector('.js-btn-reset');
+
+console.log('>> Ready :)');
