95173da3759420e07f1e827d8be2fc1cb0e2955d
ElenaFernandez <elena.fernandez92@gmail.com>
2021-12-29 12:34:30 +0100

    add function addToFav

--- src/js/main.js
+++ src/js/main.js
@@ -7,34 +7,51 @@ const btnReset = document.querySelector('.js-btn-reset');
 const animeList = document.querySelector('.js-list');
 const listFavourites = document.querySelector('.js-favourites');
 
-const DEFAULT_IMAGE ='https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+const DEFAULT_IMAGE =
+  'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
 let seriesList = [];
+let favouritesAnime = [];
 
-inputText.value='';
+inputText.value = '';
 //OBTENER INFORMACION DEL API Y GUARDARLA EN UN ARRAY
 function handleSearchSerie(event) {
   event.preventDefault();
-
   fetch(`https://api.jikan.moe/v3/search/anime?q=${inputText.value}`)
     .then((response) => response.json())
     .then((data) => {
       seriesList = data;
       printAnimeList(seriesList.results);
+
       console.log(seriesList);
-     
     });
-  console.log('>> Ready :)');
 }
+
+// PINTAR LA FUNCION EN EL HTML
 function printAnimeList(seriesList) {
-  //debugger;
   for (const eachSerie of seriesList) {
     let htmlCode = document.createElement('li');
+    htmlCode.classList.add('anime');
     animeList.appendChild(htmlCode);
-    htmlCode.innerHTML += `<img src="${eachSerie.image_url ? eachSerie.image_url : DEFAULT_IMAGE}"/>`;
+    htmlCode.innerHTML += `<img data-id="${eachSerie.mal_id}" src="${
+      eachSerie.image_url ? eachSerie.image_url : DEFAULT_IMAGE
+    }"  />`;
     htmlCode.innerHTML += `${eachSerie.title}`;
+    htmlCode.addEventListener('click', addToFav);
   }
 }
+//AÑADIR ELEMENTOS AL ARRAY FAVOURITESANIME
+function addToFav(event) {
+  const clickedId = event.target.dataset.id;
 
-
+  for (const fav of seriesList.results) {
+    if (fav.mal_id === parseInt(clickedId)) {
+      favouritesAnime.push({
+        id: `${fav.mal_id}`,
+        image: `${fav.image_url}`,
+        title: `${fav.title}`,
+      });
+    }
+  }
+}
 
 btnSearch.addEventListener('click', handleSearchSerie);
