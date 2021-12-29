85479c4ef999e1720344c596195e51dfb7696703
ElenaFernandez <elena.fernandez92@gmail.com>
2021-12-29 09:24:50 +0100

    add fetch function

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,38 @@
 'use strict';
 
 //VARIABLES GLOBALES
-let inputText = document.querySelector('.js-input-text');
+const inputText = document.querySelector('.js-input-text');
 const btnSearch = document.querySelector('.js-btn-search');
 const btnReset = document.querySelector('.js-btn-reset');
 const animeList = document.querySelector('.js-list');
 const listFavourites = document.querySelector('.js-favourites');
-let serieList = [];
 
+const DEFAULT_IMAGE ='https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+let seriesList = [];
 
-console.log('>> Ready :)');
+inputText.value='';
+//OBTENER INFORMACION DEL API Y GUARDARLA EN UN ARRAY
+function handleSearchSerie(event) {
+  event.preventDefault();
+
+  fetch(`https://api.jikan.moe/v3/search/anime?q=${inputText.value}`)
+    .then((response) => response.json())
+    .then((data) => {
+      seriesList = data;
+      console.log(seriesList);
+      printAnimeList(seriesList.results);
+    });
+  console.log('>> Ready :)');
+}
+
+function printAnimeList(seriesList) {
+  //debugger;
+  for (const eachSerie of seriesList) {
+    let htmlCode = document.createElement('li');
+    animeList.appendChild(htmlCode);
+    htmlCode.innerHTML += `<img src="${eachSerie.image_url ? eachSerie.image_url : DEFAULT_IMAGE}"/>`;
+    htmlCode.innerHTML += `${eachSerie.title}`;
+  }
+}
+
+btnSearch.addEventListener('click', handleSearchSerie);
